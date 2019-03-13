ab6a6d059daa71cdd9911c80382cc93169513e04
ovimei <ovimei123@gmail.com>
2019-03-13 16:50:06 +0700

    second

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -11,3 +11,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+
+public class Mahasiswa{
+
+    private String nama;
+    protected String npm;
+    private String nohp;
+
+    public Mahasiswa(String nama, String npm, String nohp){
+        this.nama;
+        this.npm;
+        this.nohp;
+    }
+
+    public String getNama(){
+        return nama;
+    }
+
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+
+    public String getNpm(){
+        return npm;
+    }
+
+    public void setNpm(String npm){
+        this.npm = npm;
+    }
+
+    public String getNohp(){
+        return nohp;
+    }
+
+    public void setNohp(String nohp){
+        this.nohp = nohp;
+    }
+}
